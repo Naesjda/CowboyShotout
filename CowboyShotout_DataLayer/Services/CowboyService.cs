0d3fad807ddc55a50dbeff8f06f81b73f4d27ba5
Jonathan Nash <jonathan.nash@vardehartmark.no>
2023-05-10 09:12:48 +0200

    .
    Signed-off-by: Jonathan Nash <jonathan.nash@vardehartmark.no>

--- CowboyShotout_DataLayer/Services/CowboyService.cs
+++ CowboyShotout_DataLayer/Services/CowboyService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using CowboyShotout_DataLayer.Data;
 using CowboyShotout_DataLayer.Data.CRUD;
@@ -7,14 +8,14 @@ using CowboyShotout_DataLayer.Interfaces.BaseObject;
 using CowboyShotout_DataLayer.Models.Dbo;
 using CowboyShotout_DataLayer.Models.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace CowboyShotout_DataLayer.Services;
 public class CowboyService : ICowboyService
 {
     private readonly CowboyDbContext _dbContext;
     private readonly ICRUD _crud;
-
-
+    private readonly HttpClient _httpClient = new HttpClient();
     public CowboyService(CowboyDbContext dbContext, ICRUD crud)
     {
         _dbContext = dbContext;
@@ -114,6 +115,50 @@ public class CowboyService : ICowboyService
         throw new NotImplementedException();
     }
 
+    public async Task<CowboyModel> CreateCowboyAsync()
+    {
+        var cowboy = new CowboyModel();
+
+        // Get random data for cowboy from OpenAI's GPT-3 API
+        var response = await _httpClient.PostAsync("https://api.openai.com/v1/engines/davinci-codex/completions",
+            new StringContent(JsonConvert.SerializeObject(new
+            {
+                prompt =
+                    "Generate random cowboy data\nName:\nAge:\nHeight:\nHair:\nPosition:\nHealth:\nSpeed:\nHitrate:\nGunname:\nMax bullets:\nBullets left:\n",
+                max_tokens = 1024,
+                n = 1,
+                temperature = 0.5,
+                stop = "\n"
+            })));
+
+        response.EnsureSuccessStatusCode();
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var data = JsonConvert.DeserializeObject<dynamic>(responseBody).choices[0].text.ToString().Split("\n");
+
+        cowboy.Name = GetValueFromPrompt("Name:", data);
+        cowboy.Age = int.Parse(GetValueFromPrompt("Age:", data));
+        cowboy.Height = double.Parse(GetValueFromPrompt("Height:", data));
+        cowboy.Hair = GetValueFromPrompt("Hair:", data);
+        // cowboy.Position = new CowboyModel.Position(cowboy.)
+        cowboy.Health = GetValueFromPrompt("Health:", data);
+        cowboy.Speed = double.Parse(GetValueFromPrompt("Speed:", data));
+        cowboy.HitRate = double.Parse(GetValueFromPrompt("Hitrate:", data));
+
+        cowboy.Gun = new GunModel()
+        {
+            GunName = GetValueFromPrompt("Gunname:", data),
+            MaxBullets = int.Parse(GetValueFromPrompt("Max bullets:", data)),
+            BulletsLeft = int.Parse(GetValueFromPrompt("Bullets left:", data))
+        };
+
+        return cowboy;
+    }
+
+    private string GetValueFromPrompt(string prompt, string[] data)
+    {
+        return data[Array.IndexOf(data, prompt) + 1];
+    }
     public async Task<double> DistanceBetweenCowboys(int cowboyId1, int cowboyId2)
     {
         var cowboy1 = await _crud.GetItemByIdAsync(_dbContext.CowboyModels, cowboyId1);
--- Cowboyshotout_APIs/Controllers/CowboyApi.cs
+++ Cowboyshotout_APIs/Controllers/CowboyApi.cs
@@ -15,49 +15,5 @@ public class CowboyApi
         _openaiApiKey = openaiApiKey;
     }
 
-    [HttpPost("createcowboy")]
-    public async Task<CowboyModel> CreateCowboyAsync()
-    {
-        var cowboy = new CowboyModel();
-
-        // Get random data for cowboy from OpenAI's GPT-3 API
-        var response = await _httpClient.PostAsync("https://api.openai.com/v1/engines/davinci-codex/completions",
-            new StringContent(JsonConvert.SerializeObject(new
-            {
-                prompt =
-                    "Generate random cowboy data\nName:\nAge:\nHeight:\nHair:\nPosition:\nHealth:\nSpeed:\nHitrate:\nGunname:\nMax bullets:\nBullets left:\n",
-                max_tokens = 1024,
-                n = 1,
-                temperature = 0.5,
-                stop = "\n"
-            })));
-
-        response.EnsureSuccessStatusCode();
-
-        var responseBody = await response.Content.ReadAsStringAsync();
-        var data = JsonConvert.DeserializeObject<dynamic>(responseBody).choices[0].text.ToString().Split("\n");
-
-        cowboy.Name = GetValueFromPrompt("Name:", data);
-        cowboy.Age = int.Parse(GetValueFromPrompt("Age:", data));
-        cowboy.Height = double.Parse(GetValueFromPrompt("Height:", data));
-        cowboy.Hair = GetValueFromPrompt("Hair:", data);
-        // cowboy.Position = new CowboyModel.Position(cowboy.)
-        cowboy.Health = GetValueFromPrompt("Health:", data);
-        cowboy.Speed = double.Parse(GetValueFromPrompt("Speed:", data));
-        cowboy.HitRate = double.Parse(GetValueFromPrompt("Hitrate:", data));
-
-        cowboy.Gun = new GunModel()
-        {
-            GunName = GetValueFromPrompt("Gunname:", data),
-            MaxBullets = int.Parse(GetValueFromPrompt("Max bullets:", data)),
-            BulletsLeft = int.Parse(GetValueFromPrompt("Bullets left:", data))
-        };
-
-        return cowboy;
-    }
-
-    private string GetValueFromPrompt(string prompt, string[] data)
-    {
-        return data[Array.IndexOf(data, prompt) + 1];
-    }
+    
 }
